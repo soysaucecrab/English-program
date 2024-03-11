91e7b18e11ce2735d5fd268163295bf327f718a4
soysaucecrab <88924237+soysaucecrab@users.noreply.github.com>
2024-03-12 08:05:02 +0900

    d

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:chat/screens/main_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(const MyApp());
